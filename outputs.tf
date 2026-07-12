5e20145aabff66616194200fc1128ad98528e540
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "iothub_device_update_accounts" {
-  description = "All iothub_device_update_account resources"
-  value       = azurerm_iothub_device_update_account.iothub_device_update_accounts
-}
 output "iothub_device_update_accounts_host_name" {
-  description = "List of host_name values across all iothub_device_update_accounts"
-  value       = [for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : v.host_name]
+  description = "Map of host_name values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.host_name }
 }
 output "iothub_device_update_accounts_identity" {
-  description = "List of identity values across all iothub_device_update_accounts"
-  value       = [for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : v.identity]
+  description = "Map of identity values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.identity }
 }
 output "iothub_device_update_accounts_location" {
-  description = "List of location values across all iothub_device_update_accounts"
-  value       = [for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : v.location]
+  description = "Map of location values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.location }
 }
 output "iothub_device_update_accounts_name" {
-  description = "List of name values across all iothub_device_update_accounts"
-  value       = [for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : v.name]
+  description = "Map of name values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.name }
 }
 output "iothub_device_update_accounts_public_network_access_enabled" {
-  description = "List of public_network_access_enabled values across all iothub_device_update_accounts"
-  value       = [for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : v.public_network_access_enabled]
+  description = "Map of public_network_access_enabled values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.public_network_access_enabled }
 }
 output "iothub_device_update_accounts_resource_group_name" {
-  description = "List of resource_group_name values across all iothub_device_update_accounts"
-  value       = [for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : v.resource_group_name]
+  description = "Map of resource_group_name values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.resource_group_name }
 }
 output "iothub_device_update_accounts_sku" {
-  description = "List of sku values across all iothub_device_update_accounts"
-  value       = [for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : v.sku]
+  description = "Map of sku values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.sku }
 }
 output "iothub_device_update_accounts_tags" {
-  description = "List of tags values across all iothub_device_update_accounts"
-  value       = [for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : v.tags]
+  description = "Map of tags values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.tags }
 }
 
