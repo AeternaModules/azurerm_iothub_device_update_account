c73028d3959e34bd9182c78e34e95b8d4a9793e8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "iothub_device_update_accounts_id" {
+  description = "Map of id values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.id }
+}
 output "iothub_device_update_accounts_host_name" {
   description = "Map of host_name values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
   value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.host_name }
