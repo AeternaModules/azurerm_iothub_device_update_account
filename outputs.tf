4ef1e78edb638097ca4eaeb8bbf7a72cb6accf09
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,37 @@
 output "iothub_device_update_accounts_id" {
   description = "Map of id values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.id }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "iothub_device_update_accounts_host_name" {
   description = "Map of host_name values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.host_name }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.host_name if v.host_name != null && length(v.host_name) > 0 }
 }
 output "iothub_device_update_accounts_identity" {
   description = "Map of identity values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.identity }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.identity if v.identity != null && length(v.identity) > 0 }
 }
 output "iothub_device_update_accounts_location" {
   description = "Map of location values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.location }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "iothub_device_update_accounts_name" {
   description = "Map of name values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.name }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "iothub_device_update_accounts_public_network_access_enabled" {
   description = "Map of public_network_access_enabled values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.public_network_access_enabled }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.public_network_access_enabled if v.public_network_access_enabled != null }
 }
 output "iothub_device_update_accounts_resource_group_name" {
   description = "Map of resource_group_name values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.resource_group_name }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "iothub_device_update_accounts_sku" {
   description = "Map of sku values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.sku }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.sku if v.sku != null && length(v.sku) > 0 }
 }
 output "iothub_device_update_accounts_tags" {
   description = "Map of tags values across all iothub_device_update_accounts, keyed the same as var.iothub_device_update_accounts"
-  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.tags }
+  value       = { for k, v in azurerm_iothub_device_update_account.iothub_device_update_accounts : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
